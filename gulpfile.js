44f83fa1a9c5c18047c8385641b0473bde9716f6
Alexander Tartmin <alexander@tartmin.name>
2017-03-08 14:37:57 +0200

    Reformate Gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,16 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var shell = require('gulp-shell')
-
 var sass = require('gulp-sass');
 
 
 var paths = {
-	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
-
-,
+	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json'],
 	'style': {
 		all: './public/styles/**/*.scss',
 		output: './public/styles/'
 	}
-
 };
-
 
 gulp.task('watch:sass', function () {
 	gulp.watch(paths.style.all, ['sass']);
@@ -27,12 +22,10 @@ gulp.task('sass', function(){
 		.pipe(gulp.dest(paths.style.output));
 });
 
-
 gulp.task('runKeystone', shell.task('node keystone.js'));
+
 gulp.task('watch', [
-
   'watch:sass',
-
   // 'watch:lint'
 ]);
 
