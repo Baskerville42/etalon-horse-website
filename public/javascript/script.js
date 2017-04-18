68d7050b0880623766cf7647d9f3ef3380b79d90
Alexander Tartmin <alexander@tartmin.name>
2017-04-18 20:56:13 +0300

    Some minor fixes for better results in PageSpeed

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,8 +1,1 @@
-// Main page top slider
-var topSlider = new Swiper('.top-slider.swiper-container', {
-	speed: 400,
-	spaceBetween: 0,
-	nextButton: '.top-slider .slider-button-next',
-	prevButton: '.top-slider .slider-button-prev',
-	loop: true
-});
+
